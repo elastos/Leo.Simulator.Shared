07c793465082b6ea1769acaa77491daf8956ba34
kevingzhang <kevin.zhang.canada@gmail.com>
2019-09-16 10:33:58 -0700

    move those 2 files back to node project. because they are only used in node project

--- shared/index.js
+++ shared/index.js
@@ -1,10 +1,8 @@
 const blockMgr = require("./blockMgr");
-const computeTask = require ("./computeTask");
 const constValue = require ("./constValue");
-const remoteAttestation = require ("./remoteAttestation");
 const tryVrf = require ("./tryVrf");
 const utilities = require( "./utilities");
 
 module.exports = {
-  blockMgr, computeTask, constValue, remoteAttestation, tryVrf, utilities
+  blockMgr, constValue, tryVrf, utilities
 }
